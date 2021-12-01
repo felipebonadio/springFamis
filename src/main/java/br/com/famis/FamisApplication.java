46c8229d263f0d6650780b37ff241345b956dab9
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-12-01 12:00:26 -0300

    first commit

---
+++ src/main/java/br/com/famis/FamisApplication.java
@@ -0,0 +1,13 @@
+package br.com.famis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/famis/FamisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.famis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
