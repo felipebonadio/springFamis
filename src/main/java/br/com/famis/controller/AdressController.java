f5137e13cc25564531df6dcc6ff9c43475c68725
Felipe Chagas Bonadio <felipe.bonadio@etec.sp.gov.br>
2021-12-02 11:45:28 -0300

    Adress Controller getAdress

--- src/main/java/br/com/famis/controller/AdressController.java
+++ src/main/java/br/com/famis/controller/AdressController.java
@@ -1,11 +1,28 @@
 package br.com.famis.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.famis.model.Adress;
+import br.com.famis.service.FamisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @CrossOrigin(exposedHeaders = "errors, content-type")
 @RequestMapping("api/adress")
 public class AdressController {
+
+    @Autowired
+    private FamisService famisService;
+
+    @RequestMapping(value = "/{adressId}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<Adress> getAdress(@PathVariable("adressId") UUID adressId){
+        Adress adress = this.famisService.findAdressById(adressId);
+        if(adress == null){
+            return new ResponseEntity<Adress>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<Adress>(adress, HttpStatus.OK);
+    }
 }
