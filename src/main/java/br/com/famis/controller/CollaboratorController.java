91c18d252921d4abe6f5107b2c98b928fd8ecc40
felipebonadio <febonadio@gmail.com>
2022-01-04 13:49:38 -0300

    Refatorado CollaboratorController com novas anotaçÃões

--- src/main/java/br/com/famis/controller/CollaboratorController.java
+++ src/main/java/br/com/famis/controller/CollaboratorController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,7 +22,7 @@ public class CollaboratorController {
         this.famisService = famisService;
     }
 
-    @RequestMapping(value = "/{collaboratorId}", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping("/{collaboratorId}")
     public ResponseEntity<Collaborator> getCollaborator(@PathVariable("collaboratorId") UUID collaboratorId) {
         Collaborator collaborator = this.famisService.findCollaboratorById(collaboratorId);
         if (collaborator == null) {
@@ -30,7 +31,7 @@ public class CollaboratorController {
         return new ResponseEntity<Collaborator>(collaborator, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json")
+    @GetMapping
     public ResponseEntity<List<Collaborator>> getCollaborators() {
         List<Collaborator> collaborators = this.famisService.findAllCollaborators();
         if (collaborators.isEmpty()) {
@@ -38,8 +39,9 @@ public class CollaboratorController {
         }
         return new ResponseEntity<List<Collaborator>>(collaborators, HttpStatus.OK);
     }
-    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
-    public ResponseEntity<Collaborator> saveCollaborator(@RequestBody Collaborator collaborator, BindingResult bindingResult) {
+
+    @PostMapping
+    public ResponseEntity<Collaborator> saveCollaborator(@RequestBody @Valid Collaborator collaborator, BindingResult bindingResult) {
         if(bindingResult.hasErrors() || (collaborator == null) || (collaborator.getName() == null)){
             return new ResponseEntity<Collaborator>(collaborator, HttpStatus.BAD_REQUEST);
         }
@@ -49,7 +51,7 @@ public class CollaboratorController {
         return new ResponseEntity<Collaborator>(famisService.saveCollaborator(collaborator), HttpStatus.CREATED);
     }
 
-    @RequestMapping(value = "/{collaboratorId}", method = RequestMethod.PUT, produces = "application/json")
+    @PutMapping("/{collaboratorId}")
     public ResponseEntity<Collaborator> updateCollaborator(@PathVariable("collaboratorId") UUID collaboratorId, @RequestBody Collaborator collaborator, BindingResult bindingResult){
         if(bindingResult.hasErrors() || (collaborator == null) || (collaborator.getName() == null)){
             return new ResponseEntity<Collaborator>(collaborator, HttpStatus.BAD_REQUEST);
@@ -61,7 +63,7 @@ public class CollaboratorController {
         return new ResponseEntity<Collaborator>(updatedCollaborator, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/{collaboratorId}", method = RequestMethod.DELETE, produces = "application/json")
+    @DeleteMapping("/{collaboratorId}")
     public ResponseEntity<Collaborator> deleteById(@PathVariable("collaboratorId") UUID collaboratorId) {
         Collaborator collaborator = this.famisService.findCollaboratorById(collaboratorId);
         famisService.deleteCollaborator(collaborator);
